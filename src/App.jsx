66a8945c3f50e2deb1999eae3162206153d1c1aa
shabdan <kozhonalievshabdan@gmail.com>
2023-06-20 01:40:50 +0600

    done header

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./styles/App.scss";
+import { Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/pages/Home.scss';
+import '../styles/App.scss';
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <div className="container">
+        <div className="home-content">
+            
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
