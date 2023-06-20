1c5efa205a62dbfb418184c78a90206857692f65
shabdan <kozhonalievshabdan@gmail.com>
2023-06-21 00:24:58 +0600

    added catalog component

---
+++ src/pages/Home/Catalog.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../../styles/pages/Home/Catalog.scss';
+const Catalog = () => {
+  return (
+    <div className="catalog">
+        
+    </div>
+  )
+}
+
+export default Catalog
