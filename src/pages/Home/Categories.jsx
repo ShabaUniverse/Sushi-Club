23af2a84e5a87a94fa0b20395ed9c5c2cebf5d68
shabdan <kozhonalievshabdan@gmail.com>
2023-06-21 00:25:34 +0600

    added categories component

---
+++ src/pages/Home/Categories.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../../styles/pages/Home/Categories.scss";
+
+const Categories = () => {
+  return (
+    <div className="categories">
+      <div className="categories-list">
+        <p>temaki</p>
+        <p>rice bowl</p>
+        <p>warm dishes</p>
+        <p>drinks</p>
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
