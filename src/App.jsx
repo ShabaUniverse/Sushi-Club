3f15173ece4507775cdbaa753ecace5125e36a30
shabdan <kozhonalievshabdan@gmail.com>
2023-06-26 21:23:43 +0600

    made changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import "./styles/App.scss";
 import { Route, Routes } from "react-router-dom";
+import 'react-redux';
 
+
+import "./styles/App.scss";
 import Home from "./pages/Home/Home";
 import Header from './components/Header'
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -11,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 import "../styles/components/Header.scss";
 import "../styles/App.scss";
+import Ham from "./Ham";
 
 const Header = () => {
+  const [show, setShow] = useState(false);
+
   return (
     <div className="header">
       <div className="container">
@@ -17,6 +21,27 @@ const Header = () => {
               />
             </Link>
           </div>
+
+          <div className="menu-hamburger">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              shape-rendering="geometricPrecision"
+              text-rendering="geometricPrecision"
+              image-rendering="optimizeQuality"
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              viewBox="0 0 512 351.67"
+              onClick={() => setShow(true)}
+            >
+              <path
+                fill-rule="nonzero"
+                d="M0 0h512v23.91H0V0zm0 327.76h512v23.91H0v-23.91zm0-163.88h512v23.91H0v-23.91z"
+              />
+            </svg>
+          </div>
+
+          {show && <Ham show={show} setShow={setShow}/>}
+
 
           <div className="nav">
             <ul>
@@ -32,16 +57,8 @@ const Header = () => {
             </ul>
           </div>
 
-          <div className="cart">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              id="Layer_1"
-              data-name="Layer 1"
-              viewBox="0 0 106.53 122.88"
-            >
-              <title>shopping-bag</title>
-              <path d="M4.93,30.34H27.41V25.76a25.77,25.77,0,0,1,51.53,0v4.58H101.6a4.91,4.91,0,0,1,3.47,1.45h0a4.9,4.9,0,0,1,1.44,3.48v69.9a17.75,17.75,0,0,1-17.7,17.7H17.7A17.75,17.75,0,0,1,0,105.18V35.28A4.91,4.91,0,0,1,1.45,31.8h0a4.91,4.91,0,0,1,3.47-1.45Zm28.76,0h39V25.76a19.49,19.49,0,0,0-39,0v4.58Zm-6.28,13V36.62H6.28v62h94V36.62H78.94v6.76a6.48,6.48,0,1,1-6.28-.12V36.62h-39v6.71a6.48,6.48,0,1,1-6.28,0Z" />
-            </svg>
+          <div className="reservation">
+            <a href="">make reservation</a>
           </div>
         </div>
       </div>
